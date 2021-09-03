e1d31b5875ac1c8384ccb14cabbfcb4dba4a34b9
kerin melendez <valencia.mldz13@gmail.com>
2021-09-02 18:49:41 -0600

    MI

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Grupo4_semana4_formulario
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
