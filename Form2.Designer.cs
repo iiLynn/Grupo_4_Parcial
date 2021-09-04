7663e15213d5376b617a8e2f05144f81010917bc
Estela25 <USIS035821@ugb.edu.sv>
2021-09-03 18:08:02 -0600

    form ayuda

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -29,10 +29,19 @@ namespace Grupo4_semana4_formulario
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Name = "Form2";
+            this.Text = "AYUDA";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
