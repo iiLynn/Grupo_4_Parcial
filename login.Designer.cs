ce5e897c06aef627ef21420dff7941d00d92fdfa
Estela25 <USIS035821@ugb.edu.sv>
2021-09-10 16:32:15 -0600

    arreglos

--- login.Designer.cs
+++ login.Designer.cs
@@ -112,7 +112,7 @@ namespace Grupo4_semana4_formulario
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(613, 228);
+            this.ClientSize = new System.Drawing.Size(613, 260);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
