0937c5e1e2e78722c5eb1dacf3e270fadc4bbd99
Estela25 <USIS035821@ugb.edu.sv>
2021-09-10 16:28:57 -0600

    agregando button

--- login.Designer.cs
+++ login.Designer.cs
@@ -34,6 +34,8 @@ namespace Grupo4_semana4_formulario
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -85,12 +87,34 @@ namespace Grupo4_semana4_formulario
             this.textBox2.Size = new System.Drawing.Size(141, 20);
             this.textBox2.TabIndex = 4;
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("New Comic Title Expanded Italic", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(281, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "ACCESO CON ACCESS";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("New Comic Title Expanded Italic", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(449, 184);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 32);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "ACCESO CON SQL";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(613, 228);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -112,5 +136,7 @@ namespace Grupo4_semana4_formulario
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
