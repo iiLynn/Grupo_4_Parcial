7121ec433c3bd0500520e7387ff7e9e490d8a3b9
JOSÉ LUIS MUÑOZ PADILLA <USIS043721@ugb.edu.sv>
2021-09-20 16:09:35 -0600

    login cambios

--- login.cs
+++ login.cs
@@ -14,29 +14,22 @@ namespace Grupo4_semana4_formulario
 {
     public partial class login : Form
     {
-        OleDbConnection conex = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\\Users\\orell\\OneDrive\\Escritorio\\login.accdb");
+       
         public login()
         {
             InitializeComponent();
         }
 
-        SqlConnection conexion = new SqlConnection("server=DESKTOP-PMRS7B8\\SQL4;DataBase=master; integrated security=true");
+        
         private void login_Load(object sender, EventArgs e)
         {
-            try
-            {
-                conex.Open();
-                MessageBox.Show("Conectado");
-            }
-            catch (Exception a)
-
-            {
-                MessageBox.Show("error por: " + a.ToString());
-            }
+           
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            SqlConnection conexion = new SqlConnection("server=DESKTOP-PMRS7B8\\SQL4;DataBase=master; integrated security=true");
+
             conexion.Open();
             string consulta = "Select * from ususario where ususario=´" + textBox1 + "´and contraseña=´" + textBox2 + "´";
             SqlCommand comando = new SqlCommand(consulta, conexion);
@@ -59,6 +52,19 @@ namespace Grupo4_semana4_formulario
 
         private void button1_Click(object sender, EventArgs e)
         {
+            OleDbConnection conex = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\\Users\\orell\\OneDrive\\Escritorio\\login.accdb");
+
+            try
+            {
+                conex.Open();
+                MessageBox.Show("Conectado");
+            }
+            catch (Exception a)
+
+            {
+                MessageBox.Show("error por: " + a.ToString());
+            }
+
             string consulto = "select contraseña,usuario from login where contraseña = ´" + textBox2.Text + "´ and usuario = ´" + textBox1.Text + "´;";
             OleDbCommand comande = new OleDbCommand (consulto, conex);
             OleDbDataReader leeDb;
